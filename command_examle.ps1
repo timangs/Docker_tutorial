d8bfb67e004b7945e08c86cea256e9f5e861bf16
timang <jeongkyu1145@gmail.com>
2025-03-10 18:05:31 +0900

    031017

--- command_examle.ps1
+++ command_examle.ps1
@@ -4,7 +4,7 @@ docker run -it -p 8088:8088 --name instance01 ubunut-django:1.0
 docker cp <names>:/path/requirements.txt ./requirements.txt
 docker start [<contaniner_id>|<image>]
 docker exec -it [<contaniner_id>|<image>] /bin/bash
-
+docker run -it --entrypoint /bin/bash test:test
 
 docker images
 docker ps
